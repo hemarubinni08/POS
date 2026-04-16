d603533839bf731647d8fe0fbae9914892fca840
Raja Vignesh <rajavignesh@ust.com>
2026-04-16 17:49:06 +0530

    Initial commit

--- src/main/java/com/ust/pos/HomeController.java
+++ src/main/java/com/ust/pos/HomeController.java
@@ -1,12 +1,8 @@
 package com.ust.pos;
 
-import com.ust.pos.role.service.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
