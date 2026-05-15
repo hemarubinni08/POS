a9deff777a2a00d0223eea2032572d6a1b9e8606
Bewingeo <308230@ust.com>
2026-05-15 10:13:09 +0530

    Update CategoryControllerApi.java

--- src/main/java/com/ust/pos/api/category/CategoryControllerApi.java
+++ src/main/java/com/ust/pos/api/category/CategoryControllerApi.java
@@ -14,8 +14,6 @@ import java.util.List;
 @RequestMapping("/api/category")
 public class CategoryControllerApi extends BaseController {
 
-    public static final String REDIRECT_CATEGORY_LIST = "redirect:/category/list";
-
     @Autowired
     private CategoryService categoryService;
 
