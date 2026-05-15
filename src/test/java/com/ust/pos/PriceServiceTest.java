66d83772c95fbd9a1a1945a6b82277f522d16b11
Devisetty Shashi Preetham <spdevipi@gmail.com>
2026-05-15 10:03:10 +0530

    price

--- src/test/java/com/ust/pos/PriceServiceTest.java
+++ src/test/java/com/ust/pos/PriceServiceTest.java
@@ -13,7 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
