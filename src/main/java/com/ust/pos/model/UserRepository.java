c222c3f8daa866f2ab109ed985191f38bfbb35bd
Nagarajan <Nagarajan.B@ust.com>
2026-06-01 16:56:56 +0530

    Update UserRepository.java

--- src/main/java/com/ust/pos/model/UserRepository.java
+++ src/main/java/com/ust/pos/model/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String userName);
+    User findByUsername(String username);
 
-    boolean deleteByUsername(String userName);
+    boolean deleteByUsername(String username);
 }
