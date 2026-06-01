f2535794280710581410c2220bdf2617a8bd963a
Nagarajan <Nagarajan.B@ust.com>
2026-06-01 16:52:08 +0530

    Update UserService.java

--- src/main/java/com/ust/pos/user/service/UserService.java
+++ src/main/java/com/ust/pos/user/service/UserService.java
@@ -5,13 +5,13 @@ import com.ust.pos.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
-    UserDto findByUserName(String username);
+    UserDto findByUserName(String userName);
 
     UserDto save(UserDto userDto);
 
     UserDto update(UserDto userDto);
 
-    boolean delete(String username);
+    boolean delete(String userName);
 
     List<UserDto> findAll();
 }
