455be9d4b46ebfeb694046b8abbb160518563652
Lekhya <308337@ust.com>
2026-06-04 16:09:21 +0530

    Sprint 1 files

--- src/main/java/com/ust/pos/PosApplication.java
+++ src/main/java/com/ust/pos/PosApplication.java
@@ -2,23 +2,14 @@ package com.ust.pos;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @ComponentScan({"com.ust.pos.web.controller", "com.ust.pos"})
 public class PosApplication {
-
-    @Autowired
-    Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(PosApplication.class, args);
