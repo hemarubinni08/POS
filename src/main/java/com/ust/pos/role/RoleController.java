b72bfec3cc3c8f5c3ebf490ba31afcfa2c26b176
Bewin Mathews George <308230@ust.com>
2026-06-04 13:24:57 +0530

    sprint1_mock_commit

--- src/main/java/com/ust/pos/role/RoleController.java
+++ src/main/java/com/ust/pos/role/RoleController.java
@@ -24,14 +24,14 @@ public class RoleController {
     }
 
     @GetMapping("/add")
-    public String add(Model model, @ModelAttribute RoleDto userDto) {
+    public String add(Model model, @ModelAttribute RoleDto roleDto) {
         return "role/add";
     }
 
     @PostMapping("/add")
-    public String addPost(Model model, @ModelAttribute RoleDto userDto) {
+    public String addPost(Model model, @ModelAttribute RoleDto roleDto) {
 
-        RoleDto response = roleService.save(userDto);
+        RoleDto response = roleService.save(roleDto);
         if (!response.isSuccess()) {
             model.addAttribute("error", response.getMessage());
             return "role/add";
@@ -48,12 +48,12 @@ public class RoleController {
     }
 
     @PostMapping("/update")
-    public String updatePost(Model model, @ModelAttribute RoleDto userDto) {
+    public String updatePost(Model model, @ModelAttribute RoleDto roleDto) {
 
-        RoleDto response = roleService.update(userDto);
+        RoleDto response = roleService.update(roleDto);
         if (!response.isSuccess()) {
             model.addAttribute("message", response.getMessage());
-            model.addAttribute("role", userDto);
+            model.addAttribute("role", roleDto);
             return "role/role";
         }
         return REDIRECT_ROLE_LIST;
