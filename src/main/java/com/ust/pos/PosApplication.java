ec4015193bf5c5aca33cc7f0996135fb8acea677
Logeshwaran <308343@ust.com>
2026-06-04 15:07:33 +0530

    Sprint 2 changed files

--- src/main/java/com/ust/pos/PosApplication.java
+++ src/main/java/com/ust/pos/PosApplication.java
@@ -2,19 +2,14 @@ package com.ust.pos;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan({"com.ust.pos.web.controller", "com.ust.pos"})
 public class PosApplication {
-    @Autowired
-    Environment environment;
-
     public static void main(String[] args) {
         SpringApplication.run(PosApplication.class, args);
     }
