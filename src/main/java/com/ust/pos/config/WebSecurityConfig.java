3306a36119d0e7ddb3712a27d997fa12f27b4f98
Bewin Mathews George <308230@ust.com>
2026-06-09 22:12:05 +0530

    sprint3_mock_commit

--- src/main/java/com/ust/pos/config/WebSecurityConfig.java
+++ src/main/java/com/ust/pos/config/WebSecurityConfig.java
@@ -82,7 +82,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000, http://localhost:5173")); // Allow the specific origin
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173")); // Allow the specific origin
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true); // Allow credentials if needed
