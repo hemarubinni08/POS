b1737d25d827b31398e172b3ddebbdaf27d78e8e
Anudeepika <308347@ust.com>
2026-06-26 14:43:26 +0530

    Sprint 4 commit & push

--- src/test/java/com/ust/pos/StockServiceTest.java
+++ src/test/java/com/ust/pos/StockServiceTest.java
@@ -35,21 +35,15 @@ class StockServiceTest {
     void saveTest_Success() {
         StockDto dto = new StockDto();
         dto.setIdentifier("P1");
-
         Stock entity = new Stock();
         entity.setIdentifier("P1");
-
         Mockito.when(stockRepository.findByIdentifierAndDeletedFalse("P1"))
                 .thenReturn(null);
-
         Mockito.when(modelMapper.map(dto, Stock.class))
                 .thenReturn(entity);
-
         Mockito.when(stockRepository.save(entity))
                 .thenReturn(entity);
-
         StockDto response = stockService.save(dto);
-
         Assertions.assertEquals("P1", response.getIdentifier());
         Mockito.verify(stockRepository).save(entity);
     }
@@ -131,59 +125,41 @@ class StockServiceTest {
     void findAllTest() {
         List<Stock> stocks = List.of(new Stock());
         List<StockDto> dtos = List.of(new StockDto());
-
         Type listType = new TypeToken<List<StockDto>>() {}.getType();
-
         Mockito.when(stockRepository.findByDeletedFalse())
                 .thenReturn(stocks);
-
         Mockito.when(modelMapper.map(stocks, listType))
                 .thenReturn(dtos);
-
         List<StockDto> response = stockService.findAll();
-
         Assertions.assertEquals(1, response.size());
     }
-
 
     @Test
     void findAll_WithSearch() {
         Pageable pageable = PageRequest.of(0, 10);
-
         Stock stock = new Stock();
         Page<Stock> page = new PageImpl<>(List.of(stock));
-
         StockDto dto = new StockDto();
-
         Mockito.when(stockRepository
                         .findByIdentifierContainingIgnoreCaseAndDeletedFalse("P", pageable))
                 .thenReturn(page);
-
         Mockito.when(modelMapper.map(stock, StockDto.class))
                 .thenReturn(dto);
-
         Page<StockDto> response = stockService.findAll(pageable, "P");
-
         Assertions.assertEquals(1, response.getContent().size());
     }
 
     @Test
     void findAll_WithoutSearch() {
         Pageable pageable = PageRequest.of(0, 10);
-
         Stock stock = new Stock();
         Page<Stock> page = new PageImpl<>(List.of(stock));
-
         StockDto dto = new StockDto();
-
         Mockito.when(stockRepository.findByDeletedFalse(pageable))
                 .thenReturn(page);
-
         Mockito.when(modelMapper.map(stock, StockDto.class))
                 .thenReturn(dto);
-
         Page<StockDto> response = stockService.findAll(pageable, null);
-
         Assertions.assertEquals(1, response.getContent().size());
     }
 
@@ -192,15 +168,11 @@ class StockServiceTest {
     void deleteTest() {
         Stock stock = new Stock();
         stock.setDeleted(false);
-
         Mockito.when(stockRepository.findByIdentifierAndDeletedFalse("P1"))
                 .thenReturn(stock);
-
         Mockito.when(stockRepository.save(stock))
                 .thenReturn(stock);
-
         stockService.delete("P1");
-
         Assertions.assertTrue(stock.isDeleted());
         Mockito.verify(stockRepository).save(stock);
     }
@@ -209,9 +181,7 @@ class StockServiceTest {
     void delete_WhenNotFound() {
         Mockito.when(stockRepository.findByIdentifierAndDeletedFalse("P1"))
                 .thenReturn(null);
-
         stockService.delete("P1");
-
         Mockito.verify(stockRepository, Mockito.never()).save(Mockito.any());
     }
 
